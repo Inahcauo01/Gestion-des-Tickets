6daca317c7f77c53abaf1e4e86bc5fe2503e5233
tarek <tarek.ouachani01@gmail.com>
2022-12-01 14:38:45 +0100

    landing page

---
+++ pages/conn.php
@@ -0,0 +1,17 @@
+<?php
+    $host = 'localhost';
+    $db   = '';
+    $user = 'root';
+    $pwd  = '';
+
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$db", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        // echo "Connected successfully";
+
+    }catch(PDOException $e){
+        echo "Connection failed: " . $e->getMessage();
+    }
+
+
+?>
