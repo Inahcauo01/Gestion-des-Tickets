a11a37fb730dde1de2bd08cded6d674be19c674a
tarek <tarek.ouachani01@gmail.com>
2022-12-07 17:14:16 +0100

    page reservation

--- public/pages/Matches-list-datatable.php
+++ public/pages/Matches-list-datatable.php
@@ -414,9 +414,7 @@ include '../../app/loader.php';
 													<svg width="20" height="20" viewbox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 														<path d="M17 3C17.2626 2.73735 17.5744 2.52901 17.9176 2.38687C18.2608 2.24473 18.6286 2.17157 19 2.17157C19.3714 2.17157 19.7392 2.24473 20.0824 2.38687C20.4256 2.52901 20.7374 2.73735 21 3C21.2626 3.26264 21.471 3.57444 21.6131 3.9176C21.7553 4.26077 21.8284 4.62856 21.8284 5C21.8284 5.37143 21.7553 5.73923 21.6131 6.08239C21.471 6.42555 21.2626 6.73735 21 7L7.5 20.5L2 22L3.5 16.5L17 3Z" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
 													</svg>
-												</a>
-												<a  href="../scripts.php?suppMatch=<?php echo $ ?>" id="deleteclick".$match["id"]."" hidden></a>
-          										  <button  onclick=\"confirmSupp(".$row["j_id"].")\" class=\"btn btn-sm rounded-pill\"><i class=\"fas fa-trash-alt text-secondary\"></i></a>
+												</a>	
 												<a href="javascript:void(0);" class="ml-2 btn btn-xs px-2 light btn-danger">
 													<svg width="20" height="20" viewbox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 														<path d="M3 6H5H21" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
